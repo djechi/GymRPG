83adcdf541afb78d8d8baa600795499262174770
bnmpower1 <bnmpowerislove@gmail.com>
2025-11-15 10:39:35 -0800

    Created Gemini Chatbot

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from google.genai import Client
 import os
 
 app = Flask("__name__")
 client = Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Explain how AI works in a few words",
-)
-
 # Hello world test
 @app.route("/hello")
 def helloWorld():
     return "Hello World"
 
-@app.route("/test")
-def apiTest():
-    return response.text
+@app.route("/test", methods=["POST"])
+def chatBot():
+    message = request.json.get("message")
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = message
+    )
+    return jsonify({"Response": response.text})
 
 if __name__ == "__main__":
     app.run(debug=True) 
