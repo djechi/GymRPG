a80cda512fc44ef78109120de4053b474db8e4fb
bnmpower1 <bnmpowerislove@gmail.com>
2025-11-15 14:58:03 -0800

    Condense JSON data into /home route

--- backend/server.py
+++ backend/server.py
@@ -1,39 +1,8 @@
-from flask import Flask, request, jsonify
-from google.genai import Client
-import os
-import classes
+from flask import Flask
+from homePage import homeBP
 
-app = Flask("__name__")
-client = Client(api_key=os.getenv("GEMINI_API_KEY"))
-
-# Hello world test
-@app.route("/hello")
-def helloWorld():
-    return "Hello World"
-
-# Tested in Postman and with 200 response
-@app.route("/test", methods=["POST"])
-def chatBot():
-    message = request.json.get("message")
-    response = client.models.generate_content(
-        model = "gemini-2.5-flash",
-        contents = message
-    )
-    return jsonify({"Response": response.text})
-
-@app.route("/classes")
-def allClasses():
-    return jsonify({
-        "ranger": classes.rangerClass(),
-        "thief": classes.theifClass(),
-        "warrior": classes.warriorClass(),
-        "tank": classes.tankClass()
-    })
-
-@app.route("/characterName", methods=["POST"])
-def characterName():
-    return jsonify({"Character Name": request.json.get("name")})
-
+app = Flask(__name__)
+app.register_blueprint(homeBP)
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
