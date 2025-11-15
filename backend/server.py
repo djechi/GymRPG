385adb24e48e014ef66ff3b502fc9201d26960cf
bnmpower1 <bnmpowerislove@gmail.com>
2025-11-15 09:15:56 -0800

    Implemented gemini AI response

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from google.genai import Client
+import os
 
 app = Flask("__name__")
+client = Client(api_key=os.getenv("GEMINI_API_KEY"))
 
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Explain how AI works in a few words",
+)
 
-# Hello world
+# Hello world test
 @app.route("/hello")
 def helloWorld():
     return "Hello World"
+
+@app.route("/test")
+def apiTest():
+    return response.text
 
 if __name__ == "__main__":
     app.run(debug=True) 
