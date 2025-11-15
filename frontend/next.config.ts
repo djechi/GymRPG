c0d36c7677e1bb203aad6ead37a875896d1aa974
djechi <djechipare@gmail.com>
2025-11-15 10:38:03 -0800

    Update front end with folders/files

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // Next.js automatically detects the src folder, but you can be explicit:
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 };
 
 export default nextConfig;
